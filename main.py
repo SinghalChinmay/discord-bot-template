3f62b40797999511975f319f0cb44d5f5b2c8cf6
Chinmay <chinmay.singhal08@gmail.com>
2022-10-15 01:56:40 +0530

    add some  comments

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ from os import getenv
 from dotenv import load_dotenv
 from discord.ext import commands
 
+# Loading the tokens
 load_dotenv()
 TOKEN = getenv("TOKEN")
 
 intents = discord.Intents.default()
 client = commands.Bot(command_prefix=commands.when_mentioned_or("."), intents=intents)
+client.remove_command("help")  # removing the default help command for custom one.
+
 
 @client.command()
 async def ping(ctx):
     await ctx.send("Pong!")
+
 
 client.run(TOKEN)
