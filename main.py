c524d13b539bd7d4ddb71306d3e04c6094d1d0c0
Chinmay <chinmay.singhal08@gmail.com>
2022-10-15 02:16:43 +0530

    fix: fix module error

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import discord
-from os import getenv
+import os
 from dotenv import load_dotenv
 from discord.ext import commands
 
 # Loading the tokens
 load_dotenv()
-TOKEN = getenv("TOKEN")
+TOKEN = os.getenv("TOKEN")
 
 intents = discord.Intents.default()
 client = commands.Bot(command_prefix=commands.when_mentioned_or("."), intents=intents)
