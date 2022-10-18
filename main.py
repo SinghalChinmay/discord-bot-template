bdff8da79f4adafcb367d0ab10ea849f38fbf800
Chinmay Singhal <chinmay.singhal08@gmail.com>
2022-10-18 21:04:32 +0530

    add `.gitignore`, add comments

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from discord.ext import commands
 # Loading the tokens
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
+MONGO = os.getenv("MONGO")
 
 intents = discord.Intents.default()
 client = commands.Bot(command_prefix=commands.when_mentioned_or("."), intents=intents)
@@ -13,7 +14,7 @@ client.remove_command("help")  # removing the default help command for custom on
 
 
 @client.command()
-@commands.is_owner()
+@commands.is_owner()  # Owner only command
 async def load(ctx, extension):
     client.load_extension(f"cogs.{extension}")
     await ctx.send(f"Loaded {extension} extension")
@@ -27,10 +28,14 @@ async def unload(ctx, extension):
 
 
 for folder in os.listdir("./cogs"):
-    if os.path.isdir("./cogs/" + folder) and folder.endswith("_") is False:
+    if (
+        os.path.isdir("./cogs/" + folder) and folder.endswith("_") is False
+    ):  # Searches for folders in cogs folder
         for file in os.listdir("./cogs/" + folder):
-            if file.endswith(".py"):
-                client.load_extension(f"cogs.{folder}.{file[:-3]}")
+            if file.endswith(".py"):  # Checks for python files
+                client.load_extension(
+                    f"cogs.{folder}.{file[:-3]}"
+                )  # Loads the extension
 
 
 @client.command()
